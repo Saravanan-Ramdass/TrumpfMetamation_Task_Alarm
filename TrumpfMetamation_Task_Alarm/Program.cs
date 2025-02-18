59ef5a3c112453caef3917bca0ad116eb90fe79a
Saravanan-Ramdass <rsaravanan128@gmail.com>
2025-02-18 17:01:13 +0530

    Added Odd or Even number in Program file

--- TrumpfMetamation_Task_Alarm/Program.cs
+++ TrumpfMetamation_Task_Alarm/Program.cs
@@ -1,9 +1,41 @@
 ﻿using AlarmAutomation.Tests;
-class Program
-{
+    
+class Program{
     static void Main()
     {
+        //For Alarm application
         TestAlarm testAlarm = new TestAlarm();
         testAlarm.RunTest();
+
+        //For finding even or odd number
+        var inputs = new int[] { 10, 20, 50, -1, 5, 7 };
+        IsEvenArray(inputs);
+    }
+    public static bool IsEvenNumber(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void IsEvenArray(int[] numberArray)
+    {
+        foreach (int number in numberArray)
+        {
+            if (IsEvenNumber(number))
+            {
+                Console.WriteLine(number.ToString() + " is Even");
+            }
+            else
+            {
+                Console.WriteLine(number.ToString() + " is Odd");
+            }
+        }
+
     }
 }
